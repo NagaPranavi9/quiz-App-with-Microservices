4dac8ff2d0f94d2226c608e4174c5f28020e3817
NagaPranavi9 <nagapranavimaley9@gmail.com>
2024-08-02 18:19:20 +0530

    changes

---
+++ quiz-service/src/main/java/com/example/quiz_service/model/Quiz.java
@@ -0,0 +1,26 @@
+package com.example.quiz_service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    @ElementCollection
+    private List<Integer> questionIds;
+
+    @Data
+    public static class QuizDto {
+        String categoryName;
+        Integer numQuestions;
+        String title;
+    }
+}
